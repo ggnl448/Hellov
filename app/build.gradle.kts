b33d6d46d7b489a9bc9b09942ee882f393498064
Max <ggnl0710@gmail.com>
2026-07-23 18:14:23 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.maxdev.aisearchbrowser"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.01"
+        versionCode = 3
+        versionName = "1.40"
     }
 
     buildTypes {
